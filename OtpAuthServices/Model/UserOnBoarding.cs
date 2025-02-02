71d959e496ed4af4309af86d8afdccffab820081
handyman-web <handymanserviceproviders@gmail.com>
2025-02-02 18:11:54 +0530

    adding null handling for email and iemailvalidate changes

--- OtpAuthServices/Model/UserOnBoarding.cs
+++ OtpAuthServices/Model/UserOnBoarding.cs
@@ -5,12 +5,12 @@
          public Guid UserId { get; set; }
 
         public string id { get; set; }
-        public string UserName { get; set; }
-        public string UserPassword { get; set; }
+        public string? UserName { get; set; }
+        public string? UserPassword { get; set; }
         public string MobileNo { get; set; }
-        public string EmailId { get; set; }
+        public string? EmailId { get; set; }
         public bool IsMobileNumberValidate { get; set; }
-        public bool IsEmailValidate { get; set; }
+        public bool? IsEmailValidate { get; set; }
         public string ProfileType { get; set; }
     }
 }
