11111a89fe2055ab1472cb2fcb4df532bb38c7a5
satyanarayana.krv26 <satyanarayana.krv26@gmail.com>
2025-02-25 19:10:36 +0530

    Added smsApis

--- OtpAuthServices/obj/Release/net8.0/OtpAuthServices.AssemblyInfo.cs
+++ OtpAuthServices/obj/Release/net8.0/OtpAuthServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtpAuthServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f92d0e9da0aa81c6136504737d5364a81eb56a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493b3723cb729cb22d28d8a65c709c7aa1854d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtpAuthServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtpAuthServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
