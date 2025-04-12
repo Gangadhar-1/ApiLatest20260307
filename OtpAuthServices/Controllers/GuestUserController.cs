ffe8a1017bb20df84bec2102e9b28d5471d93173
Gangadhar <gangadharpola9182@gail.com>
2025-04-12 16:28:11 +0530

    Added Get Delete Put Method to GuestUser Controller

--- OtpAuthServices/Controllers/GuestUserController.cs
+++ OtpAuthServices/Controllers/GuestUserController.cs
@@ -29,14 +29,66 @@ namespace OtpAuthServices.Controllers
             {
                 return BadRequest("GuestUser data cannot be null.");
             }
-             GuestUser.UserId = Guid.NewGuid().ToString();
-   
-             GuestUser.DateTime = DateTime.UtcNow;
+            GuestUser.UserId = Guid.NewGuid().ToString();
+
+            GuestUser.DateTime = DateTime.UtcNow;
             GuestUser.id = Guid.NewGuid().ToString();
 
             await _cosmosDbService.AddItemAsync(GuestUser);
-            return Ok(new { Message = "GuestUser Data Uploaded successfully", GuestUserId= GuestUser.UserId });
+            return Ok(new { Message = "GuestUser Data Uploaded successfully", GuestUserId = GuestUser.UserId });
         }
-    }
 
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGuestUser(string id)
+        {
+            var existingaddress = await _cosmosDbService.GetItemAsync(id);
+            if (existingaddress == null)
+            {
+                return NotFound();
+            }
+
+            await _cosmosDbService.DeleteItemAsync(id);
+            return Ok("Successfully  deleted GuestUser Data   Item. ");
+        }
+
+        
+        [HttpGet("GuestUser/{id}")]
+        public async Task<IActionResult> GetBookTechnician(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("GuestUser ID cannot be null or empty.");
+            }
+
+            var bookTechnician = await _cosmosDbService.GetItemAsync(id);
+            if (bookTechnician == null)
+            {
+                return NotFound($"GuestUser with ID {id} not found.");
+            }
+
+            return Ok(bookTechnician);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateGuestUser(string id, [FromBody] GuestUser GuestUser)
+        {
+            if (GuestUser == null || GuestUser.id != id)
+            {
+                return BadRequest("GuestUser information is incorrect.");
+            }
+
+            var existingaddress = await _cosmosDbService.GetItemAsync(id);
+            if (existingaddress == null)
+            {
+                return NotFound();
+            }
+
+            await _cosmosDbService.UpdateItemAsync(GuestUser);
+            return Ok("GuestUser data Updated successfully.");
+        }
+
+
+
+    }
 }
--- OtpAuthServices/obj/Release/net8.0/OtpAuthServices.AssemblyInfo.cs
+++ OtpAuthServices/obj/Release/net8.0/OtpAuthServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtpAuthServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266ad709dd1d874299a997976ad6c680625d8398")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14295ecb49778eae2dd5d5453077511ecfc09e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtpAuthServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtpAuthServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
